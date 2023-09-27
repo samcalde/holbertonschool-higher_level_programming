dcdad7ccc3e15eb3bde620c21c8fab0285ad9845
Samuel <samuel@iMac-de-Andres.local>
2023-09-27 01:45:38 -0500

    tasks done, test 1

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pow(a, b):
+	if (b == 0):
+		return (1)
+	result = 1
+	if (b < 0):
+		pwr = -b
+	else:
+		pwr = b
+	for i in range(1, pwr+1):
+		result = result * a
+	if (b < 0):
+		return (1/result)
+	else:
+		return (result)
