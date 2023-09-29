3b5f60e02f66d39731218c196e70228594c312e0
Samuel <samuel@iMac-de-Andres.local>
2023-09-29 01:14:27 -0500

    task 2 test 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
 import sys
-argument_lenght = len(sys.argv)
-arguments = sys.argv
 
-if (argument_lenght <= 1):
-    print("0 arguments.")
-elif (argument_lenght == 2):
-    print("{} argument:".format(argument_lenght - 1))
-else:
-    print("{} arguments:".format(argument_lenght - 1))
 
-for i in range(1, (argument_lenght)):
-    print("{}: {}".format(i, arguments[i]))
+def args_funct():
+    argument_lenght = len(sys.argv)
+    arguments = sys.argv
+
+    if (argument_lenght <= 1):
+        print("0 arguments.")
+    elif (argument_lenght == 2):
+        print("{} argument:".format(argument_lenght - 1))
+    else:
+        print("{} arguments:".format(argument_lenght - 1))
+
+    for i in range(1, (argument_lenght)):
+        print("{}: {}".format(i, arguments[i]))
+        
+
+if (__name__ == __main__):
+    args_funct()
