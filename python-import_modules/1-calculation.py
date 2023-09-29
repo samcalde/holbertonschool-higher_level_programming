c579fb8d9a170afba4749d3c362e9fd687a81469
Samuel <samuel@iMac-de-Andres.local>
2023-09-29 00:54:52 -0500

    task 1 fix 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,8 @@ calculator = __import__("calculator_1")
 a = 10
 b = 5
 
-print("{} + {} = {}".format(a, b, calculator.add(a, b)))
-print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
-print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator.div(a, b)))
+if (__name__ == "__main__"):
+    print("{} + {} = {}".format(a, b, calculator.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calculator.div(a, b)))
