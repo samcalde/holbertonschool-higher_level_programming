376baf07ae15ead47825e2b9345e1c08b5bdf4a3
Samuel <samuel@iMac-de-Andres.local>
2023-09-29 01:18:43 -0500

    task 5 test 1

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-variable_imported = __import__("variable_load_5")
-print(variable_imported.a)
+
+def var_load():
+    variable_imported = __import__("variable_load_5")
+    print(variable_imported.a)
+
+
+if (__name__ == "__main__"):
+    var_load()
