90318190521ac594e2e2e47d14cefef213519456
Samuel <samuel@iMac-de-Andres.local>
2023-09-29 01:17:14 -0500

    task 3 test 1

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+
 import sys
-argument_lenght = len(sys.argv)
-arguments = sys.argv
-total = 0
 
-if (argument_lenght > 1):
-    for i in range(1, (argument_lenght)):
-        total = total + int(arguments[i])
 
-print(total)
+def add_funct():
+    argument_lenght = len(sys.argv)
+    arguments = sys.argv
+    total = 0
+
+    if (argument_lenght > 1):
+        for i in range(1, (argument_lenght)):
+            total = total + int(arguments[i])
+
+    print(total)
+
+
+if (__name__ == "__main__"):
+    add_funct()
