34862393ccd064053be9769f940cdc332b476fcb
Samuel <samuel@iMac-de-Andres.local>
2023-09-29 13:41:53 -0500

    task 6 test 2

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 
-def calculator():
-    argument_lenght = len(sys.argv)
-    arguments = sys.argv
+if (__name__ == "__main__"):
+    argument_lenght = len(argv)
+    arguments = argv
 
     if (argument_lenght != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     elif arguments[2] not in ('+', '-', '*', '/'):
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
     else:
         a = int(arguments[1])
         b = int(arguments[3])
         if (arguments[2] == '+'):
             print("{} + {} = {}".format(a, b, add(a, b)))
         if (arguments[2] == '-'):
-            print("{} + {} = {}".format(a, b, sub(a, b)))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         if (arguments[2] == '*'):
-            print("{} + {} = {}".format(a, b, mul(a, b)))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         if (arguments[2] == '/'):
-            print("{} + {} = {}".format(a, b, div(a, b)))
-
-
-if (__name__ == "__main__"):
-    calculator()
+            print("{} / {} = {}".format(a, b, div(a, b)))
