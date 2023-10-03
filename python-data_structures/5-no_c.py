188a1b86e11c6b946e815a97c70c18332b085f01
Samuel <samuel@iMac-de-Andres.local>
2023-10-02 23:58:30 -0500

    tasks 1-6 test 1

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_str = ""
+    for i in range(0, len(my_string)):
+        if(my_string[i] not in ('c', 'C')):
+            new_str += my_string[i]
+    return(new_str)
