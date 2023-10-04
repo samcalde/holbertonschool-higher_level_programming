4c6f6835eb775d85957426f220ce56afec70af4c
Samuel <samuel@iMac-de-Andres.local>
2023-10-04 02:07:23 -0500

    tasks 7-12 test 1

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    sentence_length = len(sentence)
+    if(sentence_length > 0):
+        first_char = sentence[0]
+    else:
+        first_char = "None"
+    new_tuple = (sentence_length, first_char)
+    return(new_tuple)
