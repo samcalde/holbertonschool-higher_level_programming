be063e7717058bf807d3785fa6d05dcefebcfa20
Samuel <samuel@iMac-de-Andres.local>
2023-10-06 20:21:09 -0500

    tasks 0-2 test 1

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for item in range(0, x):
+        try:
+            print("{:d}".format(my_list[item]), end='')
+            counter += 1
+        except IndexError:
+            break
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+    print("")
+    return (counter)
