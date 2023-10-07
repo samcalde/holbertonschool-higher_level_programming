7a1e4e8868515d375f72d30234e3ee580ca7e917
Samuel <samuel@iMac-de-Andres.local>
2023-10-06 21:40:46 -0500

    tasks 3-6

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    for index in range(0, list_length):
+        try: 
+            result_list.append(my_list_1[index] / my_list_2[index])
+        except ZeroDivisionError:
+            print("division by 0")
+            result_list.append(0)
+        except TypeError:
+            print("wrong type")
+            result_list.append(0)
+        except IndexError:
+            print("out of range")
+            result_list.append(0)
+        finally:
+            continue
+    return (result_list)
---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    var = "0"
+    return (var / 1)
