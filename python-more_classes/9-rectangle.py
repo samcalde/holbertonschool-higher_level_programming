80cffcf9f9c7cde5c3edb1da62238c87b3aa85cd
Samuel <samuel@iMac-de-Andres.local>
2023-10-12 01:08:23 -0500

    task 9 test 2

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.increase_instances()
 
     @classmethod
