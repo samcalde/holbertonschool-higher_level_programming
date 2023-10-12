f3e44947055f898997c11de8af8a8298f3d8893e
samcalde <129321248+samcalde@users.noreply.github.com>
2023-10-12 01:12:19 -0500

    Update 2-square.py code style

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Creating a class that represents a square
+"""Creating a class that represents a square
 """
 
 
@@ -9,7 +8,6 @@ class Square:
     """
     The class that represents a square with a size, taking errors into account
     """
-
     def __init__(self, size=0):
         """
         Init for the square class oject
@@ -17,12 +15,10 @@ class Square:
         Args:
             size (int): size of the square
         """
-        if type(size) is int:
-            try:
-                if (size >= 0):
-                    self.__size = size
-                elif (size < 0):
-                    raise ValueError('size must be >= 0')
-        else:
+        try:
+            if (size >= 0):
+                self.__size = size
+            elif (size < 0):
+                raise ValueError('size must be >= 0')
+        except TypeError:
             raise TypeError('size must be an integer')
-
