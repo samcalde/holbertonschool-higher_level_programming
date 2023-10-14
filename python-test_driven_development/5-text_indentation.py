82bfc87511fd50cfeb400cb55207af7535bdd9f9
Samuel <samuel@iMac-de-Andres.local>
2023-10-14 00:41:44 -0500

    task 4 test 1

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+prints a text with 2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Makes 2 lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    lower_limit = 0
+    higher_limit = 0
+    i = 0
+    for character in text:
+        i += 1
+        if character in (".", "?", ":"):
+            higher_limit = i
+            print(f"{text[lower_limit:higher_limit]}\n\n")
+            lower_limit = i + 1
