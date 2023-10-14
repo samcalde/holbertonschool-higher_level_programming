bf4146beaa2921c4e96fab8efca426fd8bc344f8
Samuel <samuel@iMac-de-Andres.local>
2023-10-13 23:40:11 -0500

    task 0 test 1

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Function that adds 2 ints
+"""
+def add_integer(a, b=98):
+    """
+    Adds int a with int b
+    """
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if not (type(a) == int):
+        raise TypeError("a must be an integer")
+    if not (type(b) == int):
+        raise TypeError("b must be an integer")
+    return a + b
+    
