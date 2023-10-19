c05266b6e9a7100bac835d79847be2db2a52ed68
Samuel <samuel@iMac-de-Andres.local>
2023-10-19 01:34:16 -0500

    ad. task 2 code style fix

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Define a function that adds a new attribute to an object
+"""
+
+
 def add_attribute(obj, att_name, att_value):
+    """
+    Adds an attribute to an object after checking if it's possible
+    """
     if isinstance(obj, (str, int, float, tuple, bool, frozenset)):
         raise TypeError("can't add new attribute")
     setattr(obj, att_name, att_value)
