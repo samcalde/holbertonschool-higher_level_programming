c0acaf0fee680f8b785c461a570a54ba0b969dcc
Samuel <samuel@iMac-de-Andres.local>
2023-10-19 01:14:28 -0500

    ad. task 1 test 2

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 
+"""
+Class that inherits from int and switches == and !=
+"""
 class MyInt(int):
+    """
+    Class that inherits from int
+    """
     def __eq__(self, y):
+        """
+        Switch == for !=
+        """
         return super().__ne__(y)
 
     def __ne__(self, y):
+        """
+        Switch != for ==
+        """
         return super().__eq__(y)
