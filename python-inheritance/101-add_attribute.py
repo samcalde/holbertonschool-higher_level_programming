6ff14325178c4ee8ae20427373d8daeb8172843d
Samuel <samuel@iMac-de-Andres.local>
2023-10-19 17:32:20 -0500

    ad. task 2 test 4

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -9,9 +9,6 @@ def add_attribute(obj, att_name, att_value):
     """
     Adds an attribute to an object after checking if it's possible
     """
-    if (hasattr(obj, '__slots__') and att_name not in obj.__slots__):
+    if isinstance(obj, (str, int, float, tuple, bool, frozenset)):
         raise TypeError("can't add new attribute")
-    elif (isinstance(obj.__class__, (str, int, float, tuple, bool, frozenset))):
-        setattr(obj, att_name, att_value)
-    else:
-        raise TypeError("can't add new attribute")
+    setattr(obj, att_name, att_value)
