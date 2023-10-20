ef8f0eb3ddc95dd1906865f1ba69b2c57f535e3a
Samuel <samuel@iMac-de-Andres.local>
2023-10-20 17:12:22 -0500

    tasks 3 and 4 test 1

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Contains function that returns an object represented by a JSON string
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    Function that returns an object represented by a JSON string
+    """
+    return json.loads(my_str)
