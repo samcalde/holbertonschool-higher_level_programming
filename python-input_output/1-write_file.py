467e2e71c46ce7b963c6b230a79bcf42036d04a9
Samuel <samuel@iMac-de-Andres.local>
2023-10-20 16:29:36 -0500

    tasks 1 and 2 test 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module that writes a string to a text file (UTF8) 
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        char_written = file.write(text)
+        return char_written
---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module that appends a string at the end of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends text at the end of a file and returns the number of characters added
+    """
+    with open(filename, 'a', encoding="utf-8") as file:
+        char_added = file.write(text)
+        return char_added
