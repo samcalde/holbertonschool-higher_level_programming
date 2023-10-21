b90d605b6a806bb347898cb6f16b4408bc663cac
Samuel <samuel@iMac-de-Andres.local>
2023-10-20 19:07:31 -0500

    task 9 test 1

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Creates the class 'Student'
+"""
+
+
+class Student():
+    """
+    'Student' class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of the class
+        """ 
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """ 
+        return vars(self)
