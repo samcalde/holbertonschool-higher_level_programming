ee38949447a37feae5b2bc0cee63c92a9b21178d
Samuel <samuel@iMac-de-Andres.local>
2023-10-20 19:08:12 -0500

    task 9 code style fix

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -12,7 +12,7 @@ class Student():
     def __init__(self, first_name, last_name, age):
         """
         Instantiation of the class
-        """ 
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -20,5 +20,5 @@ class Student():
     def to_json(self):
         """
         Retrieves a dictionary representation of a Student instance
-        """ 
+        """
         return vars(self)
