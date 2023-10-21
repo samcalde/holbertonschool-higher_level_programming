a3875ab54e17ca2a12da840aa23b8316c9288b2f
Samuel <samuel@iMac-de-Andres.local>
2023-10-20 20:32:28 -0500

    task 11 test 1

---
+++ python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Creates the class 'Student'
+"""
+
+
+class Student():
+    """
+    'Student' class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        if isinstance(attrs, list):
+            if (all(isinstance(item, str) for item in attrs)):
+                dictionary = vars(self)
+                filtered_dic = {}
+                for element in attrs:
+                    if (element in dictionary):
+                        filtered_dic[element] = dictionary[element]
+                return filtered_dic
+        return vars(self)
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        """
+        for key in json:
+            setattr(self, key, json[key])
