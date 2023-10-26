c3915517cb5951d95b240319211916423989e037
Samuel <samuel@iMac-de-Andres.local>
2023-10-25 19:26:55 -0500

    tasks up to 13

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,11 +83,11 @@ class Rectangle(Base):
         """
         if type(value) != int:
             raise TypeError(f"{name} must be an integer")
-        if (name in ("y", "x")) and value < 0:
+        if (name in ("y", "x", "id")) and value < 0:
             raise ValueError(f"{name} must be >= 0")
         if (name in ("width", "height")) and value <= 0:
             raise ValueError(f"{name} must be > 0")
-    
+
     def area(self):
         """
         Returns area of the Rectangle instance
@@ -98,15 +98,43 @@ class Rectangle(Base):
         """
         Displays rectangle printed with #
         """
+        for y in range(self.__y):
+            print("")
         for height in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
             for width in range(self.__width):
                 print("#", end="")
             print("")
 
     def __str__(self):
         """
-        Replaces the str method to print [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        Returns format [Rectangle] (<id>) <x>/<y> - <width>/<height>
         """
-        line1 = f"[{self.__class__.__name__}] ({self.id}) {self.__x}/{self.__y}"
-        line2 = f" - {self.__width}/{self.__height}"
+        line1 = f"[{self.__class__.__name__}] ({self.id}) {self.__x}/"
+        line2 = f"{self.__y} - {self.__width}/{self.__height}"
         return (line1 + line2)
+    
+    def update(self, *args, **kwargs):
+        """
+        Updates values of the Rectangle
+        """
+        dictionary = {1: "id", 2: "width", 3: "height", 4: "x", 5: "y"}
+        if args:
+            i = 1
+            for argument in args:
+                self.integer_validator(dictionary[i], argument)
+                setattr(self, dictionary[i], argument)
+                i += 1
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of the instance
+        """
+        dict1 = {'id': self.id, 'width': self.width, 'height': self.height}
+        dict1['x'] = self.x
+        dict1['y'] = self.y
+        return dict1
---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+Definition of a class Square, that inherits from Rectangle
+"""
+
+
+from models.rectangle import Rectangle
+class Square(Rectangle):
+    """
+    Definition of class and methods of Square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Replaces the str method to print [Square] (<id>) <x>/<y> - <size>
+        """
+        line1 = f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y}"
+        line2 = f" - {self.width}"
+        return (line1 + line2)
+
+    @property
+    def size(self):
+        """
+        Returns size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        size setter
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Updates attributes of the Square
+        """
+        dictionary = {1: "id", 2: "size", 3: "x", 4: "y"}
+        if args:
+            i = 1
+            for argument in args:
+                self.integer_validator(dictionary[i], argument)
+                setattr(self, dictionary[i], argument)
+                i += 1
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+    
+    def to_dictionary(self)
+        """
+        Returns the dictionary representation of the instance
+        """
+        print(vars(self))
