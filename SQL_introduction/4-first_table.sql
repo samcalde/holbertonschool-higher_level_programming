00db874e844bc5504c6af84b42bcd8ecd56aa611
Samuel Calderon <s.calderon0624@gmail.com>
2023-12-19 11:45:15 -0500

    tasks full

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -2,7 +2,7 @@
 -- first_table description:
 -- id INT
 -- name VARCHAR(256)
-CREATE TABLE IF NOT EXTSTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id int,
     name VARCHAR(256)
 );
